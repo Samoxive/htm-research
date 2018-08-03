01af6241de22a83c37a6d4523024e0e620371533
Şamil NART <samil.nart@gmail.com>
2018-08-03 13:02:27 +0200

    Keep the hashed value in a virtual register instead of storing it every time to stack, now the pass actually hashes given store instruction

--- llvm_experiments/library.cpp
+++ llvm_experiments/library.cpp
@@ -6,23 +6,56 @@
 #include "llvm/IR/Constants.h"
 #include "llvm/IR/InlineAsm.h"
 #include "llvm/Transforms/IPO/PassManagerBuilder.h"
+
 using namespace llvm;
 
 const auto TM_START_FUNCTION_NAME = "__start_transaction";
 const auto TM_END_FUNCTION_NAME = "__end_transaction";
 const auto TM_GET_HASH_FUNCTION_NAME = "__get_hash";
+const auto TM_CRC32_FUNCTION_NAME = "__crc32";
 
 namespace {
     struct SamPass : public FunctionPass {
         static char ID;
+
         SamPass() : FunctionPass(ID) {}
 
+        Value *castToLong(Value *value, IRBuilder<> &builder) {
+            Value *rawIntValue;
+            auto *valueType = value->getType();
+            auto &context = value->getContext();
+            if (valueType->isIntegerTy()) {
+                if (valueType->getPrimitiveSizeInBits() < 64) {
+                    rawIntValue = builder.CreateZExt(value, IntegerType::getInt64Ty(context));
+                } else if (valueType->getPrimitiveSizeInBits() > 64) {
+                    rawIntValue = builder.CreateTrunc(value, IntegerType::getInt64Ty(context));
+                } else {
+                    rawIntValue = value;
+                }
+            } else if (valueType->isPointerTy()) {
+                rawIntValue = builder.CreatePtrToInt(value, IntegerType::getInt64Ty(context));
+            } else if (valueType->isFloatTy()) {
+                rawIntValue = builder.CreateBitCast(value, IntegerType::get(context, valueType->getPrimitiveSizeInBits()));
+                return castToLong(rawIntValue, builder);
+            }
+            return rawIntValue;
+        }
+
         virtual bool runOnFunction(Function &F) {
+            auto &context = F.getContext();
+            auto *module = F.getParent();
+            auto *crc32_function = module->getFunction(TM_CRC32_FUNCTION_NAME);
+            if (!crc32_function) {
+                errs() << "I can't do this.\n";
+                return false;
+            }
+
             bool modified = false;
-            std::vector<Instruction*> to_be_deleted;
+            std::vector<Instruction *> to_be_deleted;
             for (auto &B: F) {
                 bool in_transaction = false;
                 Value *hash_variable_stack = nullptr;
+                Value *hash_variable_reg = nullptr;
                 for (auto &I: B) {
                     auto &context = I.getContext();
                     IRBuilder<> builder(&I);
@@ -35,7 +68,9 @@ namespace {
                                 return modified;
                             }
                             hash_variable_stack = builder.CreateAlloca(IntegerType::getInt32Ty(context));
-                            builder.CreateStore(ConstantInt::get(IntegerType::getInt32Ty(context), 10, true), hash_variable_stack);
+                            builder.CreateStore(ConstantInt::get(IntegerType::getInt32Ty(context), 0, true),
+                                                hash_variable_stack);
+                            hash_variable_reg = builder.CreateLoad(hash_variable_stack);
                             modified = true;
                             in_transaction = true;
                         } else if (function_name.equals(TM_END_FUNCTION_NAME)) {
@@ -44,6 +79,16 @@ namespace {
                                 return modified;
                             }
                             in_transaction = false;
+                        } else if (function_name.equals(TM_GET_HASH_FUNCTION_NAME)) {
+                            if (hash_variable_stack == nullptr) {
+                                context.emitError("Function tried to get hash without creating a transaction!\n");
+                                return modified;
+                            }
+
+                            for (auto& U : inst->uses()) {
+                                User* user = U.getUser();
+                                user->setOperand(U.getOperandNo(), hash_variable_reg);
+                            }
                         } else {
                             continue;
                         }
@@ -53,23 +98,19 @@ namespace {
                         }
 
                         auto *value_operand = inst->getValueOperand();
-                        auto *value_type = value_operand->getType();
-                        if (!value_type->isIntegerTy()) {
-                            continue;
-                        }
-
-                        if (auto *int_type_of_value = dyn_cast<IntegerType>(value_type)) {
-                            if (int_type_of_value->getBitWidth() != 32) {
-                                continue;
-                            }
-                        } else {
-                            continue;
-                        }
-
-                        Value *hash_variable = builder.CreateLoad(hash_variable_stack);
-                        auto *hash_add_result = builder.CreateAdd(hash_variable, value_operand);
-                        builder.CreateStore(hash_add_result, hash_variable_stack);
+                        Value *args[] = {hash_variable_reg, castToLong(value_operand, builder)};
+                        hash_variable_reg = builder.CreateCall(crc32_function, args);
                         modified = true;
+                    } else if (auto *inst = dyn_cast<ReturnInst>(&I)) {
+                        if (hash_variable_stack == nullptr) {
+                            continue;
+                        }
+
+                        if (auto *ret_value = inst->getReturnValue()) {
+                            if (ret_value->getType()->isIntegerTy(32)) {
+                                inst->setOperand(0, hash_variable_reg);
+                            }
+                        }
                     }
                 }
             }
@@ -77,6 +118,7 @@ namespace {
             for (auto *I: to_be_deleted) {
                 I->removeFromParent();
             }
+
             outs() << F;
             return modified;
         }
@@ -88,4 +130,5 @@ char SamPass::ID = 0;
 static void registerSkeletonPass(const PassManagerBuilder &, legacy::PassManagerBase &PM) {
     PM.add(new SamPass());
 }
+
 static RegisterStandardPasses RegisterMyPass(PassManagerBuilder::EP_EarlyAsPossible, registerSkeletonPass);
