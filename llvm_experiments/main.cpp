bf3d85e07a652acbc36bfb562b21d590e9583e94
Şamil NART <samil.nart@gmail.com>
2018-08-03 13:03:04 +0200

    Demonstrate capabilities of the actual store hashing version, and its ability to work with different length stores

--- llvm_experiments/main.cpp
+++ llvm_experiments/main.cpp
@@ -2,9 +2,13 @@
 #include "tm.h"
 
 int main() {
-    int x = 0, hash = 0;
+    int x = 0, hash = 0, *ptr = nullptr;
     __start_transaction();
+    hash = 16;
     x = 32;
+    ptr = &x;
     __end_transaction();
+    std::cout << "Expected hash: " << __crc32(__crc32(__crc32(0, (long) hash), (long) x), (long) ptr) << std::endl;
+    std::cout << "Ptr: " << ptr << " Sum: " << ((long) ptr + (long) x + (long) hash) << " Hash: " << __get_hash();
     return x;
 }
